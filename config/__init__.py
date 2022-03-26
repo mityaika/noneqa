ed729e1317cee58ce83ffd4ea04cb16b2fd8fd37
mityaika <mityaika@gmail.com>
2022-03-26 18:34:28 -0400

    rename config file

--- config/__init__.py
+++ config/__init__.py
@@ -2,7 +2,7 @@ import configargparse
 
 parser = configargparse.get_argument_parser(name='default')
 
-parser.add_argument('--default-config', is_config_file=True, default='config/defaults.ini',
+parser.add_argument('--default-config', is_config_file=True, default='config/default.config',
                     help='default config file. default: %(default)s')
 parser.add_argument('--config-file', required=False, is_config_file=True,
                     help='environment config file', env_var='ENV_CONFIG_FILE')
