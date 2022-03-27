4c196cf105318e02d76445af19b05e85abc3ac0b
dmitrofanov <DimaAlla2021$%>
2022-03-27 18:16:36 -0400

    now find by name/id return main element

--- pom/locators.py
+++ pom/locators.py
@@ -9,13 +9,22 @@ class MainPage:
     device_capacity = {'by': By.CSS_SELECTOR, 'value': '.device-capacity'}
     device_edit = {'by': By.CSS_SELECTOR, 'value': '.device-edit'}
     device_remove = {'by': By.CSS_SELECTOR, 'value': '.device-remove'}
+    add_device_btn = {'by': By.CSS_SELECTOR, 'value': '.submitButton'}
 
     @staticmethod
     def find_device_by_name(name):
-        value = f'//span[contains(@class, "device-name") and .//text()="{name}"]'
+        # find .device-name class with text in span and go 2 levels up for the main object
+        value = f'.//span[@class="device-name" and contains(text(), "{name}")]/../..'
         return {'by': By.XPATH, 'value': value}
 
     @staticmethod
     def find_device_by_id(device_id):
-        value = f'//a[contains(@class, "device-edit") and contains(@href, "{device_id}")]'
+        value = f'//a[contains(@class, "device-edit") and contains(@href, "{device_id}")]/../..'
         return {'by': By.XPATH, 'value': value}
+
+
+class DevicePage:
+    system_name = {'by': By.ID, 'value': 'system_name'}
+    type = {'by': By.ID, 'value': 'type'}
+    hdd_capacity = {'by': By.ID, 'value': 'hdd_capacity'}
+    submit_btn = {'by': By.CSS_SELECTOR, 'value': '.submitButton'}
